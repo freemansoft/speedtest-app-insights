1f3ba414902c2e789746c6550baa13e402b1d03d
Joe Freeman <joe@freemansoft.com>
2022-11-11 16:55:43 -0500

    remove duplicat package locations

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,8 @@
+# Installer script for windows, people who don't do bash
+
 # Install components for speedtest 
 # This will route you to the store to install python if it is not installed
 python3 --version
-
-# should just use pip3 install -r requirements.txt
-# all that is required for the core part
-python3 -m pip install speedtest-cli
-python3 -m pip install opencensus-ext-azure
-python3 -m pip install dnsdiag
+python3 -m pip install -r requirements.txt
 
 # should add the setup for dns testing - see 0-setup.sh
